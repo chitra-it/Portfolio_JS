00e3e2be6d885f4cd9696e7847ca7b8546f60829
Chitra Shree K <chitrashreek0419@gmail.com>
2026-05-07 20:03:06 +0530

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,20 +1,19 @@
 /*
-  Copyright 2019 Google LLC
+  Copyright 2018 Google LLC
 
   Use of this source code is governed by an MIT-style
   license that can be found in the LICENSE file or at
   https://opensource.org/licenses/MIT.
 */
 
-import {messageSW} from './messageSW.js';
-import {Workbox} from './Workbox.js';
-
-import './_version.js';
+import {GenerateSW, GenerateSWConfig} from './generate-sw';
+import {InjectManifest} from './inject-manifest';
 
 /**
- * @module workbox-window
+ * @module workbox-webpack-plugin
  */
-export {messageSW, Workbox};
+export {GenerateSW, GenerateSWConfig, InjectManifest};
 
-// See https://github.com/GoogleChrome/workbox/issues/2770
-export * from './utils/WorkboxEvent.js';
+// TODO: remove this in v7.
+// See https://github.com/GoogleChrome/workbox/issues/3033
+export default {GenerateSW, InjectManifest};
